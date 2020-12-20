c07adb5e35e85445e12ca600d5ee2d35eae82dc6
Adam Dingle <adam@medovina.org>
2020-12-20 15:25:17 +0100

    Add Constraint.toString()

--- src/csp/Constraint.java
+++ src/csp/Constraint.java
@@ -1,6 +1,6 @@
 package csp;
 
-import java.util.List;
+import java.util.*;
 
 // A constraint that says that exactly a certain number (count) of a certain set
 // of variables are true.
@@ -10,5 +10,14 @@ public class Constraint {
 
     public Constraint(int count, List<Integer> vars) {
         this.count = count;  this.vars = vars;
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<String> varNums = new ArrayList<String>();
+        for (int v : vars)
+            varNums.add(Integer.toString(v));
+
+        return String.format("%d of {%s}", count, String.join(" ", varNums));
     }
 }
